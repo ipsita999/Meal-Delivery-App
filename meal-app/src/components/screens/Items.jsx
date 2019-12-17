9f4fbbce1699f2c470ce2bc439a3c890c750bc57
Jane Madonna <janecogger@gmail.com>
2019-12-17 11:45:56 -0500

    work on search bar

---
+++ meal-app/src/components/screens/Items.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Items = (props) => {
+    return (
+        <div>Items Page</div>
+    )
+}
+
+export default Items
---
+++ meal-app/src/components/shared/Search.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import axios from 'axios'
+import { Redirect } from 'react-router-dom'
+
+class Search extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            items: [],
+            search: ''
+        }
+    }
+
+    handleChange = e => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    handleSubmit = async event => {
+        event.preventDefault()
+        try {
+        const resp = await axios.get(`https://api.edamam.com/search?q=${this.state.search}&app_id=a658f766&app_key=404751045748e4a51448ab55aec45973`)
+        .then(resp => resp.status === 200
+            ? this.setState({items: resp.data}) && this.props.history.push('/results')
+            : null)
+        console.log(this.state.items)
+    }
+       catch (error) {
+           console.error(error)
+       }
+    }
+
+    render() {
+        return (
+            <>
+                <form onSubmit={(e) => this.handleSubmit(e)}>
+                    <input type='text' value={this.state.search} name='search' onChange={(e) => this.handleChange(e)}/>
+                    <input type='submit'/>
+                </form>
+                {this.state.items.length > 0 ? 
+                    <Redirect to='/results'/> : null
+                }
+            </>
+        )
+    }
+}
+
+export default Search
