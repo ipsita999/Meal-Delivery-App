4dc0d224f5b79a539d21fd034d4b8d2c2043b27d
Jane Madonna <janecogger@gmail.com>
2019-12-17 13:22:07 -0500

    props

--- meal-app/src/components/screens/Items.jsx
+++ meal-app/src/components/screens/Items.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Search from '../shared/Search'
 
 const Items = (props) => {
+    console.log(props)
     return (
-        <div>Items Page</div>
+        <div> Items Page
+            <Search />
+        </div>
     )
 }
 
