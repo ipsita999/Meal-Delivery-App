9abf31b04a7ae056cdc8af21fb9e115bb157f2d3
Jane Madonna <janecogger@gmail.com>
2019-12-17 12:13:15 -0500

    nav bar

--- meal-app/src/components/shared/Nav.jsx
+++ meal-app/src/components/shared/Nav.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Nav = () => {
     return (
-        <>
-        </>
+        <div className='nav-bar'>
+        <NavLink className='nav-link' to='/'>NO PREP MEALS</NavLink>
+        <p>RECIPES</p>
+        <p>ABOUT</p>
+        <p>CONTACT</p>
+        <p>MY PROFILE</p>
+        </div>
     )
 }
 
